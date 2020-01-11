41d66c4adb7a5af22904e1118856919295d734f5
Dayan Betancourt <dkbetancourt@gmail.com>
2020-01-11 17:07:23 -0400

    Componente Central (Dashboard)

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col">
+      <div class="card">
+        <div class="card-header">Bienvenido a tu Dashboard</div>
+
+        <div class="card-body">
+          <div class="container">
+            <div class="row justify-content-between">
+              <a href="#" class="btn btn-primary"><i class="fas fa-users"></i> Usuarios</a>
+              <a href="#" class="btn btn-primary"><i class="fas fa-cogs"></i> Categorías</a>
+              <a href="#" class="btn btn-primary"><i class="fas fa-tasks"></i> Tareas</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('Component Dashboard mounted.')
+  }
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">Dashboard</div>
-
-        <div class="card-body">
-          @if (session('status'))
-            <div class="alert alert-success" role="alert">
-              {{ session('status') }}
-            </div>
-          @endif
-
-          <p>Bienvenido a tu sesión</p>
-
-          <div class="container">
-            <div class="row justify-content-between">
-              <a href="#" class="btn btn-primary"><i class="fas fa-users"></i> Usuarios</a>
-              <a href="#" class="btn btn-primary"><i class="fas fa-cogs"></i> Categorías</a>
-              <a href="#" class="btn btn-primary"><i class="fas fa-tasks"></i> Tareas</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<hr />
-<example-component></example-component>
+<dashboard-component></dashboard-component>
 @endsection
